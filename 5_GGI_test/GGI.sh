00d814f91d32fdf6e0340ef16e7414d50d2f9946
Liming Cai <34404264+lmcai@users.noreply.github.com>
2020-10-09 23:19:25 -0700

    Update GGI.sh

--- 5_GGI_test/GGI.sh
+++ 5_GGI_test/GGI.sh
@@ -1,12 +1,26 @@
+export ID=$1
+
+#prepare constrained tree, remove missing taxa
+python constrained_tree_prep.py [input_tree] EuphoPeraPutraPandaER.ref.tre
+python constrained_tree_prep.py [input_tree] EuphoPeraPutraPanda.ref.tre
+python constrained_tree_prep.py [input_tree] EuphoPeraPutra.ref.tre
+python constrained_tree_prep.py [input_tree] EuphoPera.ref.tre
+python constrained_tree_prep.py [input_tree] Eupho.ref.tre
+python constrained_tree_prep.py [input_tree] Ixon.ref.tre
+python constrained_tree_prep.py [input_tree] Passi.ref.tre
+
 #run raxml on partially constrained tree
-raxmlHPC-SSE3 -p12345 -g Eupho_Raff.tre -m GTRGAMMA -s 1002.aln -n Eupho_Raff
-raxmlHPC-SSE3 -p12345 -g EuphoPera_Raff.tre -m GTRGAMMA -s 1002.aln -n EuphoPera_Raff
-raxmlHPC-SSE3 -p12345 -g EuphoPeraPutra_Raff.tre -m GTRGAMMA -s 1002.aln -n EuphoPeraPutra_Raff
-raxmlHPC-SSE3 -p12345 -g EuphoPeraPutraER_Raff.tre -m GTRGAMMA -s 1002.aln -n EuphoPeraPutraER_Raff
+raxmlHPC-SSE3 -p12345 -g $ID.Eupho.tre -m GTRGAMMA -s $ID.aln -n $ID.Eupho
+raxmlHPC-SSE3 -p12345 -g $ID.EuphoPera.tre -m GTRGAMMA -s 1002.aln -n $ID.EuphoPera
+raxmlHPC-SSE3 -p12345 -g $ID.EuphoPeraPutra.tre -m GTRGAMMA -s 1002.aln -n $ID.EuphoPeraPutra
+raxmlHPC-SSE3 -p12345 -g $ID.EuphoPeraPutraPanda.tre -m GTRGAMMA -s 1002.aln -n $ID.EuphoPeraPutraPanda
+raxmlHPC-SSE3 -p12345 -g $ID.EuphoPeraPutraPandaER.tre -m GTRGAMMA -s 1002.aln -n $ID.EuphoPeraPutraPandaER
+raxmlHPC-SSE3 -p12345 -g $ID.Ixon.tre -m GTRGAMMA -s 1002.aln -n $ID.Ixon
+raxmlHPC-SSE3 -p12345 -g $ID.Passi.tre -m GTRGAMMA -s 1002.aln -n $ID.Passi
 
 #estimate per site log-likelihood in raxml
-cat RAxML_result.* > RAxML_result.all
-raxmlHPC -f g -s 1002.aln.reduced -z RAxML_result.all -n raff.likelihood -m GTRGAMMA
+cat RAxML_result.$ID.* > RAxML_result.$ID.all
+raxmlHPC -f g -s $ID.aln.reduced -z RAxML_result.$ID.all -n $ID.likelihood -m GTRGAMMA
 
 #RAxML_perSiteLLs.*
 
