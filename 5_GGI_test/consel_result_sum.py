eefd46d7c39bbab4dc0c18bee0cdfff12a968c25
Liming Cai <34404264+lmcai@users.noreply.github.com>
2020-10-11 16:41:46 -0700

    update

--- 5_GGI_test/consel_result_sum.py
+++ 5_GGI_test/consel_result_sum.py
@@ -5,14 +5,22 @@ for fn in os.listdir('./'):
     if fnmatch.fnmatch(fn,'*.CONSEL.result'):filename.append(fn)
 
 
-output=open('AU_sum.tsv','a')
-output.write('\t'.join(['Gene','H1','H2','H4','H5','H7','H9','H12','H15','H16','H17'])+'\n')
+au_output=open('AU_sum.tsv','a')
+au_output.write('\t'.join(['Gene','H1','H2','H4','H5','H7','H9','H12','H15','H16','H17'])+'\n')
 
-output=open('WSH_sum.tsv','a')
-output.write('\t'.join(['Gene','H1','H2','H4','H5','H7','H9','H12','H15','H16','H17'])+'\n')
+pp_output=open('PP_sum.tsv','a')
+pp_output.write('\t'.join(['Gene','H1','H2','H4','H5','H7','H9','H12','H15','H16','H17'])+'\n')
+
+sh_output=open('SH_sum.tsv','a')
+sh_output.write('\t'.join(['Gene','H1','H2','H4','H5','H7','H9','H12','H15','H16','H17'])+'\n')
+
+kh_output=open('KH_sum.tsv','a')
+kh_output.write('\t'.join(['Gene','H1','H2','H4','H5','H7','H9','H12','H15','H16','H17'])+'\n')
 
 au_results={}
-wsh_result={}
+pp_results={}
+sh_results={}
+kh_results={}
 
 for fn in filename:
 	x=open(fn).readlines()
@@ -27,11 +35,20 @@ for fn in filename:
 	a2.sort()
 	y=a1+a2
 	y=[i.split('.')[1] for i in y]
-	res={}
+	au_res={}
+	pp_res={}
+	sh_res={}
+	kh_res={}
 	for l in x[3:-1]:
 		#e.g. res['H2']=0.98
-		res[y[int(l.split()[2])-1]]=l.split()[4]
-	au_results[gene]=res
+		au_res[y[int(l.split()[2])-1]]=l.split()[4]
+		pp_res[y[int(l.split()[2])-1]]=l.split()[8]
+		sh_res[y[int(l.split()[2])-1]]=l.split()[10]
+		kh_res[y[int(l.split()[2])-1]]=l.split()[9]
+	au_results[gene]=au_res
+	pp_results[gene]=pp_res
+	sh_results[gene]=sh_res
+	kh_results[gene]=kh_res
 
 
 for key in au_results.keys():
@@ -41,7 +58,30 @@ for key in au_results.keys():
 			result=result+au_results[key][i]+'\t'
 		except KeyError:
 			result=result+'NA'+'\t'
-	output.write(result+'\n')
+	au_output.write(result+'\n')
+	result=key+'\t'
+	for i in ['H1','H2','H4','H5','H7','H9','H12','H15','H16','H17']:
+		try:
+			result=result+pp_results[key][i]+'\t'
+		except KeyError:
+			result=result+'NA'+'\t'
+	pp_output.write(result+'\n')
+	result=key+'\t'
+	for i in ['H1','H2','H4','H5','H7','H9','H12','H15','H16','H17']:
+		try:
+			result=result+sh_results[key][i]+'\t'
+		except KeyError:
+			result=result+'NA'+'\t'
+	sh_output.write(result+'\n')
+	result=key+'\t'
+	for i in ['H1','H2','H4','H5','H7','H9','H12','H15','H16','H17']:
+		try:
+			result=result+kh_results[key][i]+'\t'
+		except KeyError:
+			result=result+'NA'+'\t'
+	kh_output.write(result+'\n')
 
-
-output.close()
+au_output.close()
+pp_output.close()
+sh_output.close()
+kh_output.close()
