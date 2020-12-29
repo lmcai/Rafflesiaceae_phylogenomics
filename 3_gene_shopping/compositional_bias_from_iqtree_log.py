6dc5c7271813356c0aab2bc3fe7b64fd25d26f25
Liming Cai <daybreak.chua@gmail.com>
2020-12-28 17:48:39 -0800

    Update compositional_bias_from_iqtree_log.py

--- 3_gene_shopping/compositional_bias_from_iqtree_log.py
+++ 3_gene_shopping/compositional_bias_from_iqtree_log.py
@@ -6,18 +6,21 @@ import os
 
 files=os.listdir('.')
 files=[i for i in files if i.endswith('.log')]
-out=open('na_aln_chisquare_compositional_bias.tsv')
+out=open('na_aln_chisquare_compositional_bias.tsv','a')
 out.write('GeneID\tRafflesiaceae_best_compositional_homogeneity_pvalue\n')
 for i in files:
 	x=open(i).readlines()
 	j=18
+	cur_best=0
 	while True:
-		cur_best=0
 		ll=x[j].split()
-		if ll[0]=='WARNING:':break
+		if ll[0]=='WARNING:' or ll[0]=='****':break
 		if ll[1] in ['Sap','Rca','Rtu','Rhi']:
-			if float(ll.split()[-1][:-1])>cur_best:
-				cur_best=float(ll.split()[-1][:-1])
+			if float(ll[-1][:-1])>cur_best:
+				print j
+				cur_best=float(ll[-1][:-1])
+				print cur_best
+		j=j+1
 	out.write(i.split('.')[0]+'\t'+str(cur_best)+'\n')
 
 out.close()
