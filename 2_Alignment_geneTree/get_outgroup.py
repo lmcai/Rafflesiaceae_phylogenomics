262fdb67296b4268b7e083eadbf0bdbc6019072a
Liming Cai <daybreak.chua@gmail.com>
2024-08-20 16:03:48 -0400

    Update get_outgroup.py

--- 2_Alignment_geneTree/get_outgroup.py
+++ 2_Alignment_geneTree/get_outgroup.py
@@ -4,4 +4,4 @@ x=[l[1:].strip() for l in x if l.startswith('>')]
 outgr=[]
 for candidate in ['Vitis','Elaeocarpus','Crossopetalum','Oxalis']:
 	outgr = outgr + [leaf for leaf in x if leaf.startswith(candidate)]
-print outgr[0]
+print(outgr[0])
