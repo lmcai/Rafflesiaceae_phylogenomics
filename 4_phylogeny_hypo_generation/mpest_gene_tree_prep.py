965f7fb9847990c628f07d32da041ab632602c45
Liming Cai <daybreak.chua@gmail.com>
2024-08-21 12:58:48 -0400

    Update mpest_gene_tree_prep.py

--- 4_phylogeny_hypo_generation/mpest_gene_tree_prep.py
+++ 4_phylogeny_hypo_generation/mpest_gene_tree_prep.py
@@ -1,11 +1,32 @@
 from ete3 import Tree
-x=open('G2299.gene.trees').readlines()
+import ete3
+x=open('G2141.aster.genetree.trees').readlines()
 
-out=open('G2299.mpest.gene.trees','a')
+out=open('G2141.mpest.gene.trees','a')
 trees=[]
 for l in x:
 	t=Tree(l.strip())
-	trees.append(t.write(format=9))
+	sp=[node.name for node in t]
+	try:
+		raff=set(sp) & {'Sap','Rhi','Rca','Rtu'}
+		if len(raff)>1:
+			temroot=t.get_common_ancestor(raff)
+			t.set_outgroup(temroot)
+		else:
+			t.set_outgroup(list(raff)[0])
+		if 'Oxalis' in sp and 'Elaeocarpus' in sp:
+			root=t.get_common_ancestor('Oxalis','Elaeocarpus')
+			t.set_outgroup(root)
+		elif 'Oxalis' in sp:
+			t.set_outgroup(t&"Oxalis")
+		elif 'Elaeocarpus' in sp:
+			t.set_outgroup(t&"Elaeocarpus")
+		elif 'Crossopetalum' in sp:
+			t.set_outgroup(t&"Crossopetalum")
+		trees.append(t.write(format=9))
+	except:
+		print(t.write())
+
 
 out.write('\n'.join(trees))
 out.close()
